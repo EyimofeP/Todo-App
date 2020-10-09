dfe53a508c89b70bd7870bd803d8cbf3a818fd95
Eyimofe Ayo Pinnick <eyimofe36@gmail.com>
2020-10-09 09:38:07 +0100

    Solved Server Error Problem

--- todo/settings.py
+++ todo/settings.py
@@ -78,11 +78,13 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'eyimofe36',
+        'HOST' : 'localhost'
     }
 }
-
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
