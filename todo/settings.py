610ddfe2df5e2f79e89e4a1a02abc497e97d830b
Eyimofe Ayo Pinnick <eyimofe36@gmail.com>
2020-11-02 01:35:59 +0100

    Hid Some Secrets

--- todo/settings.py
+++ todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uxb4i79(2(is@x#86u9)*+%lj_*3e&^2&h8v!yegg^=@uwetr7'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
